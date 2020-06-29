ebf3665a2789decbb6d7ee59a9c7edc90a08b59f
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-06-29 06:00:48 +0200

    Reviews Component

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Start Change Backgorund Color Component
 
 const changeBgContainer = document.querySelector(".change-bg-container");
-const bgColor = document.querySelector(".bg-color");
 const bgColorTitle = document.querySelector(".bg-color-title");
+const bgColor = document.querySelector(".bg-color");
 const changeBgBtn = document.querySelector(".change-bg-btn");
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
@@ -15,9 +15,9 @@ function changeBgColor() {
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
   }
+  bgColorTitle.style.color = hexColor;
   bgColor.textContent = hexColor;
   bgColor.style.color = hexColor;
-  bgColorTitle.style.color = hexColor;
   changeBgContainer.style.backgroundColor = hexColor;
 }
 
@@ -31,26 +31,32 @@ const counterValue = document.querySelector(".counter-value");
 const counterBtns = document.querySelectorAll(".counter-btn");
 let count = 0;
 
+function changeCounterValue(event) {
+  if (event.target.innerText === "DECREASE") {
+    count = count - 1;
+  } else if (event.target.innerText === "INCREASE") {
+    count = count + 1;
+  } else {
+    count = 0;
+  }
+
+  if (count < 0) {
+    counterValue.style.backgroundColor = "red";
+  } else if (count > 0) {
+    counterValue.style.backgroundColor = "green";
+  } else {
+    counterValue.style.backgroundColor = "#ff6663";
+  }
+
+  counterValue.innerText = count;
+}
+
 counterBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (event) {
-    if (event.target.innerText === "DECREASE") {
-      count = count - 1;
-    } else if (event.target.innerText === "RESET") {
-      count = 0;
-    } else if (event.target.innerText === "INCREASE") {
-      count = count + 1;
-    }
-
-    counterValue.innerText = count;
-
-    if (count < 0) {
-      counterValue.style.backgroundColor = "red";
-    } else if (count > 0) {
-      counterValue.style.backgroundColor = "green";
-    } else {
-      counterValue.style.backgroundColor = "#ff6663";
-    }
-  });
+  btn.addEventListener("click", changeCounterValue);
 });
 
 // End Counter Component
+
+// Start Reviews Component
+
+// End Reviews Component
