6b418c7c1ecd887fcb676db1add0279e00bbb9ae
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-06-30 06:37:45 +0200

    Reviews Component

--- js/main.js
+++ js/main.js
@@ -59,4 +59,85 @@ counterBtns.forEach(function (btn) {
 
 // Start Reviews Component
 
+// Data
+
+const reviewItems = [
+  {
+    id: 1,
+    name: "max dell",
+    job: "UX designer",
+    img: "images/reviews/1.png",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, perspiciatis sint officiis accusantium a fugiat! Ipsa cumque nemo dolores fugit",
+  },
+  {
+    id: 2,
+    name: "john doe",
+    job: "web developer",
+    img: "images/reviews/2.png",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, perspiciatis sint officiis accusantium ",
+  },
+  {
+    id: 3,
+    name: "kobe byre",
+    job: "copywriting",
+    img: "images/reviews/3.png",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, perspiciatis sint officiis accusantium a fugiat! Ipsa cumque nemo ",
+  },
+  {
+    id: 4,
+    name: "karole deek",
+    job: "digital marketer",
+    img: "images/reviews/4.png",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing  perspiciatis sint officiis accusantium a fugiat! Ipsa cumque nemo dolores fugit",
+  },
+];
+
+// Variables
+
+const reviewItem = document.querySelector(".review-item");
+const reviewImage = document.querySelector(".review-image > img");
+const reviewAuthor = document.querySelector(".review-author");
+const reviewJob = document.querySelector(".review-job");
+const reviewInfo = document.querySelector(".review-info ");
+
+const prevButton = document.querySelector(".prev-btn");
+const nextButton = document.querySelector(".next-btn");
+
+let currentItem = 0;
+
+// Manipulating Data
+
+function showPerson() {
+  const item = reviewItems[currentItem];
+  reviewImage.src = item.img;
+  reviewAuthor.textContent = item.name;
+  reviewJob.textContent = item.job;
+  reviewInfo.textContent = item.text;
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+prevButton.addEventListener("click", function () {
+  if (currentItem > 0) {
+    currentItem--;
+  } else {
+    currentItem = reviewItems.length - 1;
+  }
+  showPerson();
+});
+nextButton.addEventListener("click", function () {
+  if (currentItem < reviewItems.length - 1) {
+    currentItem++;
+  } else {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
 // End Reviews Component
